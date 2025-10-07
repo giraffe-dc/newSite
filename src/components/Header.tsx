4fbe0fcc2290d7795b795924be17b85efef02ab2
IvanPustovit <pustovit.for@gmail.com>
2025-10-07 17:24:36 +0300

    mobstyle

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,37 +8,56 @@ const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   return (
-    <header className={styles.header}>
-      <div className="container">
-        <div className={styles.headerContent}>
-          <Link href="/" className={styles.logo}>
-            <div className={styles.logoIcon} style={{ backgroundImage: `url(${logo.src})` }}></div>
-            {/* <span className={styles.logoText}>Жирафик</span> */}
-          </Link>
-          
-          <nav className={`${styles.nav} ${isMenuOpen ? styles.navOpen : ''}`}>
-            <Link href="/" className={styles.navLink}>
-              Головна
-            </Link>
-            <Link href="/prices" className={styles.navLink}>
-              Наші ціни
-            </Link>
-            <Link href="/news-contacts" className={styles.navLink}>
-              Новини та контакти
-            </Link>
-          </nav>
+      <header className={styles.header}>
+          <div className="container">
+              <div className={styles.headerContent}>
+                  <Link href="/" className={styles.logo}>
+                      <div
+                          className={styles.logoIcon}
+                          style={{ backgroundImage: `url(${logo.src})` }}
+                      ></div>
+                      {/* <span className={styles.logoText}>Жирафик</span> */}
+                  </Link>
 
-          <button 
-            className={styles.menuToggle}
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
-          >
-            <span></span>
-            <span></span>
-            <span></span>
-          </button>
-        </div>
-      </div>
-    </header>
+                  <nav
+                      className={`${styles.nav} ${
+                          isMenuOpen ? styles.navOpen : ''
+                      }`}
+                  >
+                      <Link
+                          href="/"
+                          className={styles.navLink}
+                          onClick={() => setIsMenuOpen(false)}
+                      >
+                          Головна
+                      </Link>
+                      <Link
+                          href="/prices"
+                          className={styles.navLink}
+                          onClick={() => setIsMenuOpen(false)}
+                      >
+                          Наші ціни
+                      </Link>
+                      <Link
+                          href="/news-contacts"
+                          className={styles.navLink}
+                          onClick={() => setIsMenuOpen(false)}
+                      >
+                          Новини та контакти
+                      </Link>
+                  </nav>
+
+                  <button
+                      className={styles.menuToggle}
+                      onClick={() => setIsMenuOpen(!isMenuOpen)}
+                  >
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                  </button>
+              </div>
+          </div>
+      </header>
   )
 }
 
